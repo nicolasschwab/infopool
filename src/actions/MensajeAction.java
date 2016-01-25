c3f1ad25227649b39242e749445d6db99f23af00
NicolasSchwab <nicolasschwab2@hotmail.com>
2016-01-25 02:08:38 -0800

    se limpio la clase, se eliminaron los metodos y varibles que ya no servian

--- src/actions/MensajeAction.java
+++ src/actions/MensajeAction.java
@@ -4,10 +4,9 @@ import implementacionesDAO.FactoryDAO;
 import implementacionesDAO.ForoMensajesDAOjpa;
 import implementacionesDAO.MensajeDAOjpa;
 import implementacionesDAO.ViajeDAOjpa;
-import implementacionesDAO.ViajeroDAOjpa;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
@@ -37,35 +36,15 @@ public class MensajeAction extends ActionSupport {
 	private String detalle;
 	private String estado;	
 	private Usuario emisor;
-	private Usuario receptor;
 	private MensajeDAOjpa mensajeDAO;
-	private List<Mensaje> mensajeLista = new ArrayList<Mensaje>();
 	private ViajeDAOjpa viajeDAO=(ViajeDAOjpa) FactoryDAO.getViajeDAO();
 	private List<Viajero> listaReceptores ;
-	private ViajeroDAOjpa viajeroDAO = (ViajeroDAOjpa) FactoryDAO.getViajeroDAO();
 	private ForoMensajesDAOjpa foroMensajeDAO=(ForoMensajesDAOjpa) FactoryDAO.getForoMensajesDAO();
-	private String receptorID;
 	private List<Viajero> lista;
 	private int viajeId;
 	private String respDetalle;
-	private String queNoSos;
-	private String notif="";
 	private Usuario user;
 	
-	
-	
-	public String getNotif() {
-		return notif;
-	}
-	public void setNotif(String notif) {
-		this.notif = notif;
-	}
-	public String getQueNoSos() {
-		return queNoSos;
-	}
-	public void setQueNoSos(String queNoSos) {
-		this.queNoSos = queNoSos;
-	}
 	public String getRespDetalle() {
 		return respDetalle;
 	}
@@ -83,12 +62,6 @@ public class MensajeAction extends ActionSupport {
 	}
 	public void setLista(List<Viajero> lista) {
 		this.lista = lista;
-	}
-	public String getReceptorID() {
-		return receptorID;
-	}
-	public void setReceptorID(String receptorID) {
-		this.receptorID = receptorID;
 	}
 	public List<Viajero> getListaReceptores() {
 		return listaReceptores;
@@ -131,33 +104,14 @@ public class MensajeAction extends ActionSupport {
 	}
 	public void setEmisor(Usuario emisor) {
 		this.emisor = emisor;
-	}
-	public Usuario getReceptor() {
-		return receptor;
-	}
-	public void setReceptor(Usuario receptor) {
-		this.receptor = receptor;
 	}	
-	/*public String listarMensajes(){
-		Map<String, Object> session = ActionContext.getContext().getSession();
-		Usuario user = (Usuario) session.get("usrLogin");
-		mensajeLista = mensajeDAO.listar(user);
-		return SUCCESS;
-	}*/
 	public MensajeDAOjpa getMensajeDAO() {
 		return mensajeDAO;
 	}
 	public void setMensajeDAO(MensajeDAOjpa mensajeDAO) {
 		this.mensajeDAO = mensajeDAO;
 	}
-	public List<Mensaje> getMensajeLista() {
-		return mensajeLista;
-	}
-	public void setMensajeLista(List<Mensaje> mensajeLista) {
-		this.mensajeLista = mensajeLista;
-	}
-	public String verDetalleMensaje(){
-		
+	public String verDetalleMensaje(){		
 		return SUCCESS;
 	}
 	public String nuevoMensaje(){	
@@ -166,8 +120,7 @@ public class MensajeAction extends ActionSupport {
 		this.setViajeId(Integer.parseInt(elId[0]));
 		this.listaUsuarios();
 		return SUCCESS;
-	}
-	
+	}	
 	
 	private void listaUsuarios(){
 		Map<String, Object> session = ActionContext.getContext().getSession();
@@ -182,125 +135,7 @@ public class MensajeAction extends ActionSupport {
 			}
 						
 		}
-	}
-	
-	/*public String enviarMensaje(){
-		
-		Map<String, Object> session = ActionContext.getContext().getSession();
-		Usuario user = (Usuario) session.get("usrLogin");
-		if (user != null) {
-			if (user.getPerfil().equals("viajero")) {				
-				if (this.receptorID!=null && !this.receptorID.isEmpty()&& this.asunto!=null && !this.asunto.isEmpty() && this.detalle!=null && !this.detalle.isEmpty() ){
-					this.receptor=viajeroDAO.encontrar(Integer.parseInt(receptorID));
-					this.fecha=new Date();
-					this.estado="pendiente";
-					this.emisor=user;
-					Mensaje nuevoMensaje= new Mensaje (this.fecha, this.asunto,this.detalle,this.estado,this.emisor);
-					this.mensajeDAO.registrar(nuevoMensaje);
-					NotificacionAction notificacion=new NotificacionAction();
-					this.mensajeLista=this.mensajeDAO.listar(user);
-					notificacion.crearNotificacionPrivado(this.receptor, this.mensajeLista.get(this.mensajeLista.size()-1).getId());
-					return SUCCESS;	
-				}else{
-					this.listaUsuarios();
-					addFieldError("MensajeError", "Debe completar todos los campos");
-					return INPUT;
-				}
-				
-			}		
-			else {
-				return "sinpermisos";
-			}
-		} else {
-			return "login";
-		}
-	}
-	
-	
-	public String detalle(){
-		Map<String, Object> session = ActionContext.getContext().getSession();
-		Usuario user = (Usuario) session.get("usrLogin");
-		if (user != null) {
-			if (user.getPerfil().equals("viajero")) {
-				HttpServletRequest request = (HttpServletRequest) ActionContext.getContext().get(ServletActionContext.HTTP_REQUEST);
-				String[] elId= request.getParameterValues("id");
-				this.id=Integer.parseInt(elId[0]);
-				Mensaje elMensaje;
-				elMensaje=this.mensajeDAO.encontrar(this.id);
-				//cheque que el emisor o el receptor sea el ususario logueado
-				//para que alguien no pueda cambiar el ID y ver cualquier mensaje
-				if(elMensaje.getEmisor().getId()==user.getId() )
-				{				
-					if(elMensaje.getEmisor().getId()!=user.getId()){ // setea datos particulares para ver el mensaje siendo el receptor
-						this.queNoSos="Emisor";					
-						this.emisor=elMensaje.getEmisor();	
-						this.receptorID=String.valueOf(elMensaje.getEmisor().getId());
-						if(elMensaje.getEstado().equals("pendiente" )){
-							elMensaje.setEstado("leido");
-							
-							this.mensajeDAO.modificar(elMensaje);
-						}
-						
-					}else{    // setea datos particulares para  ver el mensaje siendo el emisor
-						this.queNoSos="Receptor";
-						this.emisor=elMensaje.getReceptor();					
-						}
-					this.asunto=elMensaje.getAsunto();    // setea datos en comun entre emisor y receptor
-					this.detalle=elMensaje.getDetalle();
-					this.estado=elMensaje.getEstado();				
-					if(elMensaje.isMensaje_sistema()){   // si es un mensaje enviado automaticamente por el sistema
-						this.queNoSos="Emisor";
-					}				
-					if(this.getNotif()!=""){
-						new NotificacionAction().cambiarEstadoAVisitado(this.notif);
-					}
-					
-					return SUCCESS;
-				}
-				else{
-					return "sinpermisos";
-				}
-			}		
-			else {
-				return "sinpermisos";
-			}
-		} else {
-			return "login";
-		}
-
-	}
-	*/
-	
-	
-	
-	public String responder(){
-		Map<String, Object> session = ActionContext.getContext().getSession();
-		Usuario user = (Usuario) session.get("usrLogin");
-		if (user != null) {
-			if (user.getPerfil().equals("viajero")) {
-				this.fecha=new Date();
-				this.receptor=this.viajeroDAO.encontrar(Integer.parseInt(this.receptorID));
-				this.emisor=user;
-				this.estado="pendiente";
-				Mensaje nuevoMensaje= new Mensaje (this.fecha,this.respDetalle,this.estado,this.emisor);
-				this.mensajeDAO.registrar(nuevoMensaje);
-				Mensaje elMensaje;
-				elMensaje=this.mensajeDAO.encontrar(this.id);		
-				if(elMensaje.getEstado().equals("leido")){
-					elMensaje.setEstado("respondido");
-					this.mensajeDAO.modificar(elMensaje);
-				}
-				return SUCCESS;
-			}		
-			else {
-				return "sinpermisos";
-			}
-		} else {
-			return "login";
-		}
-	}
-
-	
+	}	
 	public String nuevoMensajeForo(){
 		String resul=this.validarSesion();
 		if(resul.equals(SUCCESS)){
