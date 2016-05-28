6622b320b3f81c66dc018cdcb8b7fda0a0d6f201
NicolasSchwab <nicolasschwab2@hotmail.com>
2016-05-27 20:39:02 -0800

    Se agrego la librea Dozer para mapear DTO Tambien se agregó el mapeo a dtoViajero

---
+++ src/dto/ViajeroDto.java
@@ -0,0 +1,65 @@
+package dto;
+
+public class ViajeroDto {
+
+	private String usuario;
+	
+	private String clave;
+	
+	private String nombre;	
+
+	private String apellido;
+	
+	private float calificacion;
+	
+	private String mail;
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public float getCalificacion() {
+		return calificacion;
+	}
+
+	public void setCalificacion(float calificacion) {
+		this.calificacion = calificacion;
+	}
+	
+}
---
+++ src/util/Dozer.java
@@ -0,0 +1,16 @@
+package util;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+
+public class Dozer {
+
+	static Mapper mapper=null;
+	
+	public Mapper getMapper(){
+		if(mapper==null){
+			mapper = new DozerBeanMapper();
+		}
+		return mapper;
+	}
+}
